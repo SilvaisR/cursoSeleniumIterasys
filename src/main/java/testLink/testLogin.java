4d36c0b1698d8900cc04198e7f1d36af1be7e49a
Silvanis <silvanis.silva@outlook.com>
2022-06-10 16:11:35 -0300

    Initial commit

---
+++ src/main/java/testLink/testLogin.java
@@ -0,0 +1,18 @@
+// Pacote - package
+package testLink;
+ // Bibliotecas
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+// Classe
+public class testLogin {
+    // Atributos
+    private static WebDriver driver;
+
+    // Métodos
+    public void testaLogin(){
+        driver = new ChromeDriver();
+    }
+
+}
